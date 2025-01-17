523187cdb558856ac4d08bdf730104bc563f4446
hazemmuuhammed <202027220@std.sci.cu.edu.eg>
2025-01-17 18:28:20 +0200

    Add loading spinner with Suspense to MovieDetails page for improved user experience

--- src/app/(main)/movie-details/[imdbID]/page.tsx
+++ src/app/(main)/movie-details/[imdbID]/page.tsx
@@ -1,9 +1,15 @@
+import React, { Suspense } from "react";
 import MovieDetails from "@/components/movieDetails/MovieDetails";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function Page({
   params,
 }: {
   params: Promise<{ imdbID: string }>;
 }) {
-  return <MovieDetails params={params} />;
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <MovieDetails params={params} />
+    </Suspense>
+  );
 }
